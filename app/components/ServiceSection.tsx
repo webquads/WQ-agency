897a216144858748a66ecd7010efc3deb876f2b3
foysal767 <uddinmohammadfoysal981@gmail.com>
2025-07-21 23:07:10 +0600

    section add

---
+++ app/components/ServiceSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ServiceSection = () => {
+  return (
+    <div>
+      <h1>Service section</h1>
+    </div>
+  );
+};
+
+export default ServiceSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import ContactSection from "./components/ContactSection";
+import ServiceSection from "./components/ServiceSection";
+
 export default function Home() {
   return (
     <div>
       <h1>Web Quads</h1>
       <h3>Where Ideas Meet Innovation</h3>
+      <ServiceSection />
+      <ContactSection />
     </div>
   );
 }
