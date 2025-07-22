7f9914228b0e9e156120d35e3585eb4693c66847
foysal767 <uddinmohammadfoysal981@gmail.com>
2025-07-22 22:45:58 +0600

    section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import ContactSection from "@/components/ContactSection/ContactSection";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import NavMenu from "@/components/Navbar/NavMenu";
+import ServiceSection from "@/components/ServiceSection/ServiceSection";
 
 export default function Home() {
   return (
@@ -7,6 +9,9 @@ export default function Home() {
       <NavMenu />
 
       <HeroSection />
+
+      <ServiceSection />
+      <ContactSection />
     </div>
   );
 }
