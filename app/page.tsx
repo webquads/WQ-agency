a2512e4d8b87ca327de5c52c805110564ba19420
raihan-778 <raihanarif778@gmail.com>
2025-07-30 19:56:55 +0600

    pricing section modified

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
-import WebquadsLogo from "./ui/components/LoadingAnimation/WebquadsAnimation";
+import ContactSection from "@/components/ContactSection/ContactSection";
+import HeroSection from "@/components/HeroSection/HeroSection";
+// import ScrollNavbar from "@/components/Navbar/ScrollNavbar";
+// import NavBarJs from "@/components/Navbar/NavBarJs";
+// import Navbar from "@/components/Navbar/NavMenu";
+// import NavbarMotion from "@/components/Navbar/NavbarMotion";
+import ServiceSection from "@/components/ServiceSection/ServiceSection";
+import TableShapeBackground from "@/components/TableShapeBackground";
+import TeamMembers from "@/components/TeamMembers/TeamMembers";
+import AnimatedLogo from "./ui/components/LoadingAnimation/WebquadsAnimation";
 import PricingSection from "./ui/components/PricingSection/PricingSection";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Web Quads</h1>
+    <>
+      {/* <ScrollNavbar /> */}
+      {/* <NavBarJs /> */}
+      {/* <NavbarMotion /> */}
+      {/* <Navbar /> */}
+      <HeroSection />
+      <ServiceSection />
+      <ContactSection />
+      <TeamMembers />
+      <TableShapeBackground />
       <PricingSection />
-      <WebquadsLogo />
-    </div>
+      <AnimatedLogo />
+    </>
   );
 }
