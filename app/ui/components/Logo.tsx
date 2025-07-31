c1f5c8696b110f4dc89e722c6184deca8aa48ab1
sumaiya ahmed <sumu0612@gmail.com>
2025-07-31 20:33:12 +0600

    logo updatedd

--- app/ui/components/Logo.tsx
+++ app/ui/components/Logo.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import logo2 from "../public/images/webQuads_logo-Q2.png";
-import logo from "../public/images/webQuads_logoQ.png";
+import logo2 from "../../../public/images/webQuads_logo-Q2.png";
+import logo from "../../../public/images/webQuads_logoQ.png";
 
 export default function Logo() {
   return (
